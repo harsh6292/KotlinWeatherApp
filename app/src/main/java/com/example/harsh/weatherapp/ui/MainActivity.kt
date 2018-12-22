e27fed03625333ce95232cab917b94f87aa34b3f
Harsh Aggarwal <harshv.6292@gmail.com>
2018-12-21 23:28:13 -0500

    Use Kotlin Android Extensions to find views automatically in activity.

--- app/src/main/java/com/example/harsh/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/harsh/weatherapp/ui/MainActivity.kt
@@ -3,10 +3,9 @@ package com.example.harsh.weatherapp.ui
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import com.example.harsh.weatherapp.R
 import com.example.harsh.weatherapp.domain.commands.RequestForecastCommand
-import com.example.harsh.weatherapp.domain.model.ModelForecast
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.longToast
 import org.jetbrains.anko.toast
@@ -14,21 +13,17 @@ import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
-    private val url = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
-            "APPID=15646a06818f61f7b8d7823ca833e1ce&q=94043&mode=json&units=metric&cnt=7"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
-        forecastList.layoutManager = LinearLayoutManager(this)
+        forecast_list.layoutManager = LinearLayoutManager(this)
 
         doAsync {
             val result = RequestForecastCommand("27606").execute()
             uiThread {
                 longToast("Request Performed!")
-                forecastList.adapter = ForecastListAdapter(result) { toast("Date is ${it.date}") }
+                forecast_list.adapter = ForecastListAdapter(result) { toast("Date is ${it.date}") }
             }
         }
     }
